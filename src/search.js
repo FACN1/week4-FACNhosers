ab40d26f5c5f2a34adbe0e0b81e8856f6d581050
suha-edris <suha.edris@gmail.com>
2017-03-14 18:00:21 +0200

    updated search function

--- src/search.js
+++ src/search.js
@@ -7,17 +7,21 @@ fs.readFile('./dict.txt', function(err, data){
   }
   else{
     var dict = (data.toString()).toLowerCase().split("\n");
-    // dict = dict.split("\n")
     console.log(dict);
   }
 })
 
 function search(userInput){
   var searchResults = [];
-  dict.filter(function(element){
-    if(element.startsWith(userInput)){
-      searchResults.push(element);
+  var resultCounter =0;
+  for(var i=0;i<dict.length;i++){
+    if(dict[i].startsWith(userInput)){
+      searchResults.push(dict[i]);
+      resultCounter ++;
+      if(resultCounter === 5){
+        break;
+      }
     }
-  });
-  return searchResults.splice(0,5);
+  }
+  return searchResults;
 }
