682953b1f0adf4e8e0f021ba80dd855c45b29b6b
HilbertSpitzer <HilbertSpitzer@users.noreply.github.com>
2017-03-14 17:32:55 +0200

    Update main.js
    remove extra tab

--- client/main.js
+++ client/main.js
@@ -1,15 +1,15 @@
 var eventListeners = {}
 
 eventListeners.searchBox = function(callback){
-        var sBox = document.querySelector('#sBox');
-        sBox.addEventListener('keyup',function(event){
-                callback(sBox);
-            })
-    }
+    var sBox = document.querySelector('#sBox');
+    sBox.addEventListener('keyup',function(event){
+            callback(sBox);
+        })
+}
 
 eventListeners.logKey = function(node){
-      console.log(node.value)
-  };
+  console.log(node.value)
+};
 
 
 
