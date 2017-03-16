a179f6d11c720d7e402c7c94270e675ecb5ce28d
Matt Davis <matthewpeterdavis@gmail.com>
2017-03-16 12:39:42 +0200

    fix client side test

--- client/eventListener.js
+++ client/eventListener.js
@@ -2,7 +2,7 @@ var eventListenerModule = (function(){
 
 var sBox = document.querySelector('#sBox');
 
- function searchBox(callback){
+function searchBox(callback){
   sBox.addEventListener('keyup',function(event){
     callback('POST','/makeRequest',renderModule.renderSearch, sBox);
     })
--- client/test/test.js
+++ client/test/test.js
@@ -3,12 +3,12 @@ QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
 
-QUnit.test('test,addEventListener for keyup in the searchbox', function (assert){
+QUnit.test('test, addEventListener for keyup in the searchbox', function (assert){
   var sBox = document.querySelector('#sBox');
   var keypressed=false;
-  assert.equal(keypressed, false, 'check initially false')
+  assert.deepEqual(keypressed, false, 'check initially false')
 
-  eventListeners.searchBox(function(){
+  eventListenerModule.searchBox(function(){
     console.log('pressed')
     keypressed=true;
   })
